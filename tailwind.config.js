0577a34e4a21664230d9660b7a29b452c30a4dd2
Midhun Suresh <84007961+Midhun455@users.noreply.github.com>
2026-03-27 16:01:48 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,7 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      animation: {
-        'pulse-slow': 'pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
